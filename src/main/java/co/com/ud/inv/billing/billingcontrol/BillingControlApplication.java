ec43ae6774e8319f787d922752a413835d6fd38f
Alejandro Tique <alejandrotique@cbit-online.com>
2020-07-13 20:15:43 -0500

    Init project billing control

---
+++ src/main/java/co/com/ud/inv/billing/billingcontrol/BillingControlApplication.java
@@ -0,0 +1,13 @@
+package co.com.ud.inv.billing.billingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ud/inv/billing/billingcontrol/BillingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ud.inv.billing.billingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
